a4dfc36a79ffcd0f79b1022be4488c1c3041071a
Gabor Nyeki <gabor.nyeki@alumni.duke.edu>
2023-04-23 14:47:16 -0400

    setup.py: bump version number to 1.3

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r", encoding="utf-8") as handle:
 
 setup(
     name="tzconv",
-    version="1.2",
+    version="1.3",
     description="command-line tool to convert a date and time to several time zones at once",
     long_description=readme,
     long_description_content_type="text/markdown",
